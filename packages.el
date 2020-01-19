e3a6f6ea872e943d22734bc7eed011b2c3835cb0
Seong Yong-ju <sei40kr@gmail.com>
2020-01-20 00:46:42 +0900

    feat: rollback default snippets to doom-snippets

--- packages.el
+++ packages.el
@@ -22,7 +22,6 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
-(package! yasnippet-snippets)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
@@ -40,7 +39,6 @@
 ;; If you'd like to disable a package included with Doom, for whatever reason,
 ;; you can do so here with the `:disable' property:
 ;(package! builtin-package :disable t)
-(package! doom-snippets :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
