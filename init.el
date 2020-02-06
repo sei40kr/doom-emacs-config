cb8f294d25a444156fad254b360c6e468f5c2265
Seong Yong-ju <sei40kr@gmail.com>
2020-02-07 08:17:58 +0900

    chore: use normal modeline since +light was buggy

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
        hl-todo              ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides      ; highlighted indent columns
-       (modeline +light)    ; snazzy, Atom-inspired modeline, plus API
+       modeline             ; snazzy, Atom-inspired modeline, plus API
        nav-flash            ; blink the current line after jumping
        ;;neotree            ; a project drawer, like NERDTree for vim
        ophints              ; highlight the region an operation acts on
