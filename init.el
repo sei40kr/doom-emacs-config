8e62a7065f1a8c5a9b74ad78b4d40819e2758a3a
Seong Yong-ju <sei40kr@gmail.com>
2020-02-10 13:54:48 +0900

    feat: use +lsp flag for docker module

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
        ansible
        debugger              ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       docker
+       (docker +lsp)
        editorconfig          ; let someone else argue about tabs vs spaces
        ein                   ; tame Jupyter notebooks with emacs
        eval                  ; run code, run (also, repls)
