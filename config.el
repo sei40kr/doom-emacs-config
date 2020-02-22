4c4fcb9159bbed4698ab8e0e37b20215b72609e8
Seong Yong-ju <sei40kr@gmail.com>
2020-02-22 16:54:40 +0900

    feat: use the same flycheck display function for lsp

--- config.el
+++ config.el
@@ -451,6 +451,8 @@
     (setq-hook! 'lsp-mode-hook
       company-idle-delay 0.0
       company-minimum-prefix-length 1))
+  (after! lsp-ui
+    (setq lsp-ui-sideline-show-diagnostics nil))
 
   ;; LSP + Doom Themes
   (defun +custom--pick-doom-color (key)
