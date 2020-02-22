5c53d18fb943c093a881aed6393827bb93cbf821
Seong Yong-ju <sei40kr@gmail.com>
2020-02-22 14:21:21 +0900

    feat: improve centaur-tabs buffer grouping

--- config.el
+++ config.el
@@ -114,13 +114,36 @@
 
 (when (featurep! :ui tabs)
   (after! centaur-tabs
+    (defun +custom--centaur-tabs-buffer-groups ()
+      (cond ((or (string-equal "*" (substring (buffer-name) 0 1))
+                 (memq major-mode '(magit-blame-mode
+                                    magit-blob-mode
+                                    magit-diff-mode
+                                    magit-file-mode
+                                    magit-log-mode
+                                    magit-process-mode
+                                    magit-status-mode))) '("Emacs"))
+            ((memq major-mode '(help-mode helpful-mode)) '("Help"))
+            ((memq major-mode '(org-mode
+                                diary-mode
+                                org-agenda-log-mode
+                                org-bullets-mode
+                                org-cdlatex-mode
+                                org-agenda-clockreport-mode
+                                org-agenda-mode
+                                org-beamer-mode
+                                org-indent-mode
+                                org-src-mode)) '("Org"))
+            (t (centaur-tabs-projectile-buffer-groups))))
+
     ;; list only workspace buffers
-    (setq centaur-tabs-buffer-list-function '+workspace-buffer-list
-          centaur-tabs-buffer-groups-function 'centaur-tabs-projectile-buffer-groups)
+    (setq centaur-tabs-buffer-list-function #'+workspace-buffer-list
+          centaur-tabs-buffer-groups-function #'+custom--centaur-tabs-buffer-groups
+          centaur-tabs-close-button "×")
 
     (map! :map centaur-tabs-mode-map
-          :g "M-<left>"  'centaur-tabs-backward-tab
-          :g "M-<right>" 'centaur-tabs-forward-tab)))
+          "M-<left>"  #'centaur-tabs-backward-tab
+          "M-<right>" #'centaur-tabs-forward-tab)))
 
 
 ;; ui/treemacs
