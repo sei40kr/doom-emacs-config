2f9989de7beff77d4dc7c30e4c4c393c15bcb086
Seong Yong-ju <sei40kr@gmail.com>
2020-05-01 12:44:14 +0900

    feat: enable electric module

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
 
        :emacs
        (dired +icons)        ; making dired pretty [functional]
-       ;;electric            ; smarter, keyword-based electric-indent
+       electric              ; smarter, keyword-based electric-indent
        ;;ibuffer             ; interactive buffer management
        vc                    ; version-control and Emacs, sitting in a tree
 
