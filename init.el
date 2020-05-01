3be0afb0f7c06d110dddcd0de8eb6835f864f373
Seong Yong-ju <sei40kr@gmail.com>
2020-05-01 12:46:09 +0900

    feat: enable python poetry support

--- init.el
+++ init.el
@@ -152,7 +152,8 @@
        plantuml              ; diagrams for confusing people more
        ;;purescript          ; javascript, but functional
        (python               ; beautiful is better than ugly
-        +lsp)
+        +lsp
+        +poetry)
        ;;qt                  ; the 'cutest' gui framework ever
        ;;racket              ; a DSL for DSLs
        rest                  ; Emacs as a REST client
