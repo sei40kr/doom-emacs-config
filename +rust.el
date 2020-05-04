dab1277c97e400191ccab0123c4dfc2cf03be543
Seong Yong-ju <sei40kr@gmail.com>
2020-05-04 10:04:18 +0900

    refactor: minor refactoring

--- +rust.el
+++ +rust.el
@@ -5,10 +5,10 @@
         lsp-rust-server 'rust-analyzer
         lsp-rust-clippy-preference "on")
 
-  (defun +my-lsp/lsp--suggest-project-root-a ()
+  (defun +my-rust/lsp-mode--lsp--suggest-project-root-a ()
     "Get project root."
     (when (and (eq major-mode 'rustic-mode) (featurep 'projectile))
       (projectile-root-bottom-up (file-truename default-directory)
                                  `("Cargo.toml" @,projectile-project-root-files-bottom-up))))
   (advice-add 'lsp--suggest-project-root
-              :before-until #'+my-lsp/lsp--suggest-project-root-a))
+              :before-until #'+my-rust/lsp-mode--lsp--suggest-project-root-a))
