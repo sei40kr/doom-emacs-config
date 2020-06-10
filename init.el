3caa7890d06309822f4f8b12e4cd0299a4aac0c6
Seong Yong-ju <sei40kr@gmail.com>
2020-06-10 10:54:58 +0900

    chore: update module name: perl -> raku

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
         ;;+pomodoro          ; be fruitful with the tomato technique
         ;;+present           ; using org-mode for presentations
         )
-       perl                  ; write code no one else can comprehend
+       raku                  ; write code no one else can comprehend
        (php +lsp)            ; perl's insecure younger brother
        plantuml              ; diagrams for confusing people more
        ;;purescript          ; javascript, but functional
