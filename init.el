9a30fb4de6d710c20d1c774413e2fc8c6e647577
Seong Yong-ju <sei40kr@gmail.com>
2020-12-29 01:06:15 +0900

    Enable :emacs/undo

--- init.el
+++ init.el
@@ -69,6 +69,7 @@
        (dired +icons)        ; making dired pretty [functional]
        electric              ; smarter, keyword-based electric-indent
        ;;ibuffer             ; interactive buffer management
+       undo                  ; persistent, smarter undo for your inevitable mistakes
        vc                    ; version-control and Emacs, sitting in a tree
 
        :term
