ec47ddf3c5106d49cb3a0457d626998148c9036b
Seong Yong-ju <sei40kr@gmail.com>
2021-03-20 03:32:21 +0900

    Minor improvements for jupyter

--- contrib/org-jupyter.el
+++ contrib/org-jupyter.el
@@ -1,6 +1,7 @@
 ;;; $DOOMDIR/contrib/org-jupyter.el -*- lexical-binding: t; -*-
 
 (setq-hook! 'jupyter-org-interaction-mode-hook
+  company-minimum-prefix-length 1
   company-idle-delay 0.2)
 
 (defadvice! +org-jupyter--org-dwim-at-point-a (&optional _)
@@ -8,5 +9,4 @@
   (when-let ((info (org-babel-get-src-block-info t))
              ((string-prefix-p "jupyter-" (car info))))
     (jupyter-org-execute-and-next-block)
-    (forward-line)
     t))
