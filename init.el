ddced3066dda46d056dfe5b84ac14eb4989bbe3e
Seong Yong-ju <sei40kr@gmail.com>
2021-07-11 02:05:39 +0900

    Disable :lang org +hugo

--- init.el
+++ init.el
@@ -153,7 +153,7 @@
        (org                  ; organize your plain life in plain text
         +dragndrop           ; drag & drop files/images into org buffers
         +gnuplot
-        +hugo                ; use Emacs for hugo blogging
+        ;;+hugo              ; use Emacs for hugo blogging
         +jupyter             ; ipython/jupyter support for babel
         ;;+pandoc            ; export-with-pandoc support
         ;;+pomodoro          ; be fruitful with the tomato technique
