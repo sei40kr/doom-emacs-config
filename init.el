dd11be8b9a0984ad055106a017ab18eada5bcf2f
Seong Yong-ju <sei40kr@gmail.com>
2021-07-11 02:06:28 +0900

    Remove deprecated :ui fill-column

--- init.el
+++ init.el
@@ -30,7 +30,6 @@
        doom-dashboard        ; a nifty splash screen for Emacs
        ;;doom-quit           ; DOOM quit-message prompts when you quit Emacs
        (emoji +github)       ; 🙂
-       fill-column           ; a `fill-column' indicator
        hl-todo               ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides       ; highlighted indent columns
