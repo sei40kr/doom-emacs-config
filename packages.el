f0b827c47599a57d70cb00274c87ef3e4bfba0af
Seong Yong-ju <sei40kr@gmail.com>
2021-07-17 18:39:11 +0900

    Disable ddskk

--- packages.el
+++ packages.el
@@ -55,6 +55,7 @@
 ;; If you'd like to disable a package included with Doom, for whatever reason,
 ;; you can do so here with the `:disable' property:
 ;(package! builtin-package :disable t)
+(package! ddskk :disable t)
 (package! osx-trash :disable t)
 (package! pangu-spacing :disable t)
 
