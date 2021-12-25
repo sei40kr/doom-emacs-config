6169ce366d16af19526d6538ae2d5028178a0657
Seong Yong-ju <sei40kr@gmail.com>
2021-12-26 01:21:24 +0900

    perf(magit): improve diff performance

--- contrib/magit.el
+++ contrib/magit.el
@@ -3,9 +3,24 @@
 (setq magit-clone-set-remote\.pushDefault t
       magit-repolist-columns '(("Name" 25 magit-repolist-column-ident nil)
                                ("Version" 25 magit-repolist-column-version nil)
-                               ("Path" 99 magit-repolist-column-path nil))
+                               ("Path" 99 magit-repolist-column-path nil)))
+
+
+;;
+;;; Performance
+
+(setq magit-refresh-status-buffer nil
+      ;; Diff Performance
+      magit-diff-highlight-indentation nil
+      magit-diff-highlight-trailing nil
+      magit-diff-paint-whitespace nil
+      magit-diff-highlight-hunk-body nil
+      magit-diff-refine-hunk nil
       magit-revision-insert-related-refs nil)
 
 (after! magit
+  ;; Refs Buffer Perfomance
   (remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
+
+  ;; Committing Performance
   (remove-hook 'server-switch-hook 'magit-commit-diff))
