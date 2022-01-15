846dd5de5a90c04d5b5bb27d078d1d6486b0a7c1
Seong Yong-ju <sei40kr@gmail.com>
2022-01-15 21:05:38 +0900

    bump: :tools tree-sitter
    emacs-tree-sitter/elisp-tree-sitter@2acca5c8d2e3 -> emacs-tree-sitter/elisp-tree-sitter@48b06796a3b2

--- modules/tools/tree-sitter/packages.el
+++ modules/tools/tree-sitter/packages.el
@@ -1,4 +1,4 @@
 ;;; $DOOMDIR/modules/tools/tree-sitter/packages.el -*- no-byte-compile: t; -*-
 
-(package! tree-sitter :pin "2acca5c8d2e3dc66d4d0a99831b33140b5a5f973")
-(package! tree-sitter-langs :pin "2b845a70080c0edd66f13200b9dc8d6d0c3f42ce")
+(package! tree-sitter :pin "48b06796a3b2e76ce004972d929de38146eafaa0")
+(package! tree-sitter-langs :pin "3c0c82f9fb0a796f5ebd7e1e4c89f13d5ab6ef58")
