baf2cfe823c664545f5dcde93f8079e9eb4e5999
Seong Yong-ju <sei40kr@gmail.com>
2022-08-11 03:00:43 +0900

    refactor(config): migrate init & packages & config

--- init.el
+++ init.el
@@ -15,8 +15,10 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi                ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        japanese
+       ;;layout              ; auie,ctsrnm is the superior home row
 
        :completion
        (company +childframe) ; the ultimate code completion backend
@@ -33,6 +35,8 @@
        hl-todo               ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        ;;indent-guides       ; highlighted indent columns
+       ;;ligatures           ; ligatures and symbols to make your code pretty again
+       ;;minimap             ; show a map of the code on the side
        (modeline +light)     ; snazzy, Atom-inspired modeline, plus API
        nav-flash             ; blink the current line after jumping
        ;;neotree             ; a project drawer, like NERDTree for vim
@@ -40,12 +44,11 @@
        (popup                ; tame sudden yet inevitable temporary windows
         +all                 ; catch all popups that start with an asterix
         +defaults)           ; default popup rules
-       ;;pretty-code         ; replace bits of code with pretty symbols
        spacemacs-layouts
        tabs                  ; an tab bar for Emacs
        (treemacs +lsp)       ; a project drawer, like neotree but cooler
        ;;unicode             ; extended unicode support for various languages
-       vc-gutter             ; vcs diff in the fringe
+       (vc-gutter +pretty)   ; vcs diff in the fringe
        ;;vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        window-select         ; visually switch windows
        workspaces            ; tab emulation, persistence & separate workspaces
@@ -63,8 +66,8 @@
        ;;parinfer            ; turn lisp into python, sort of
        ;;rotate-text         ; cycle region at point between text candidates
        snippets              ; my elves. They type so I don't have to
-       spacemacs-editing
        word-wrap             ; soft wrapping with language-aware indent
+       spacemacs-editing
 
        :emacs
        (dired                ; making dired pretty [functional]
@@ -89,13 +92,13 @@
 
        :tools
        ansible
+       ;;biblio              ; Writes a PhD for you (citation needed)
        (debugger +lsp)       ; FIXME stepping through code, to help you add bugs
        ;;direnv
        (docker +lsp)
        editorconfig          ; let someone else argue about tabs vs spaces
        ein                   ; tame Jupyter notebooks with emacs
        eval                  ; run code, run (also, repls)
-       ghq
        ;;gist                ; interacting with github gists
        (lookup               ; helps you navigate your code and documentation
         +docsets             ; ...or in Dash docsets locally
@@ -113,6 +116,7 @@
        tmux                  ; an API for interacting with tmux
        tree-sitter
        ;;upload              ; map local to remote projects via ssh/ftp
+       ghq
 
        :os
        (:if IS-MAC macos)    ; improve compatibility with macOS
@@ -120,6 +124,7 @@
 
        :lang
        ;;agda                ; types of types of types of types...
+       ;;beancount           ; mind the GAAP
        (cc +lsp)             ; C/C++/Obj-C madness
        ;;clojure             ; java with a lisp
        ;;common-lisp         ; if you've seen one lisp, you've seen them all
@@ -127,26 +132,31 @@
        ;;crystal             ; ruby at the speed of c
        ;;csharp              ; unity, .NET, and mono shenanigans
        data                  ; config/data formats
+       ;;(dart +flutter)     ; paint ui and not much else
+       ;;dhall
        ;;elixir              ; erlang done right
        ;;elm                 ; care for a cup of TEA?
        emacs-lisp            ; drown in parentheses
        ;;erlang              ; an elegant language for a more civilized age
        (ess +lsp)            ; emacs speaks statistics
+       ;;factor
        ;;faust               ; dsp, but you get to keep your soul
+       ;;fortran             ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;;fsharp              ; ML stands for Microsoft's Language
+       ;;fstar               ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript            ; the language you waited for
        (go +lsp)             ; the hipster dialect
-       (haskell              ; a language that's lazier than I am
-        +lsp)
+       ;;(graphql +lsp)      ; Give queries a REST
+       (haskell +lsp)        ; a language that's lazier than I am
        ;;hy                  ; readability of scheme w/ speed of python
-       ;;idris               ;
+       ;;idris               ; a language you can depend on
+       (json +lsp)           ; At least it ain't XML
        (java +lsp)           ; the poster child for carpal tunnel syndrome
        (javascript +lsp)     ; all(hope(abandon(ye(who(enter(here))))))
-       (json +lsp)           ; At least it ain't XML
        ;;julia               ; a better, faster MATLAB
        (kotlin +lsp)         ; a better, slicker Java(Script)
        (latex +lsp)          ; writing papers in Emacs has never been so fun
        ;;lean
-       ;;factor
        ;;ledger              ; an accounting system in Emacs
        (lua +lsp)            ; one-based indices? one-based indices
        (markdown +grip)      ; writing docs for people to ignore
@@ -163,7 +173,6 @@
         ;;+pomodoro          ; be fruitful with the tomato technique
         ;;+present           ; using org-mode for presentations
         )
-       raku                  ; write code no one else can comprehend
        (php +lsp)            ; perl's insecure younger brother
        plantuml              ; diagrams for confusing people more
        ;;purescript          ; javascript, but functional
@@ -173,6 +182,7 @@
         +poetry)
        ;;qt                  ; the 'cutest' gui framework ever
        ;;racket              ; a DSL for DSLs
+       raku                  ; write code no one else can comprehend
        rest                  ; Emacs as a REST client
        ;;rst                 ; ReST in peace
        (ruby                 ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
@@ -180,26 +190,26 @@
         +rails)
        (rust +lsp)           ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        (scala +lsp)          ; java, but good
-       ;;scheme              ; a fully conniving family of lisps
+       ;;(scheme +guile)     ; a fully conniving family of lisps
        (sh                   ; she sells {ba,z,fi}sh shells on the C xor
         +fish
         +lsp)
        solidity              ; do you need a blockchain? No.
        ;;swift               ; who asked for emoji variables?
        ;;terra               ; Earth and Moon in alignment for performance.
-       (web                  ; the tubes
-        +css
-        +html
-        +lsp)
+       (web +lsp)            ; the tubes
        (yaml +lsp)           ; JSON, but readable
+       ;;zig                 ; C, but simpler
 
        :email
-       ;;(mu4e +gmail)
+       ;;(mu4e +org +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
        ;;calendar
+       ;;emms
+       ;;everywhere          ; *leave* Emacs!? You must be joking
        ;;irc                 ; how neckbeards socialize
        ;;(rss +org)          ; emacs as an RSS reader
        ;;twitter             ; twitter client https://twitter.com/vnought
